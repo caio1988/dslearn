a4f76f16b2827a5ad7693495d96f1a1263a5e2fd
caio1988 <caio.orlandini@hotmail.com>
2022-01-03 23:00:39 -0300

    Lessons, task, Content and Seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -35,3 +35,19 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 
 INSERT INTO tb_enrollment (user_Id, offer_Id, enroll_Moment, refound_Moment, available, only_Update) VALUES (1, 1, '2021-07-14T13:00:00Z', null, true, false);
 INSERT INTO tb_enrollment (user_Id, offer_Id, enroll_Moment, refound_Moment, available, only_Update) VALUES (2, 1, '2021-07-14T13:00:00Z', null, true, false);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 - Capitulo 1', 1, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'Material de apoio do capitulo 1 da aula 1', 'https://www.youtube.com/watch?v=iDhg2CtRSII');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 2 - Capitulo 1', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, 'Material de apoio do capitulo 2 da aula 1', 'https://www.youtube.com/watch?v=iDhg2CtRSII');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 3 - Capitulo 1', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, 'Material de apoio do capitulo 3 da aula 1', 'https://www.youtube.com/watch?v=iDhg2CtRSII');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa do Capitulo 1', 4, 1);
+INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Fazer o trabalho da aula', 5, 4, 1.0, 
+TIMESTAMP WITH TIME ZONE '2022-11-25T13:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1 );
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1 );
